b876eff8e89a81455979f649251cdc4768cf0f39
Christian Voegl <cvoegl@suse.com>
2021-06-14 19:19:14 +0200

    Allow machines to be entered without a MAC address
    This Allows the machine to be entered without a MAC address, and also
    enables the cobbler backend to deal with those machines.
    
    This change is necessary as for many machines a machine with BMC entry
    has to be created in orthos2, in order to discover the machines MAC
    address

--- orthos2/data/models/machine.py
+++ orthos2/data/models/machine.py
@@ -19,7 +19,7 @@ from .architecture import Architecture
 from .domain import Domain, DomainAdmin, validate_domain_ending
 from .enclosure import Enclosure
 from .machinegroup import MachineGroup
-from .networkinterface import validate_mac_address
+from .networkinterface import validate_mac_address, NetworkInterface
 from .platform import Platform
 from .system import System
 from .virtualizationapi import VirtualizationAPI
@@ -492,6 +492,12 @@ class Machine(models.Model):
 
     mac_address = None
 
+    unknown_mac = models.BooleanField(
+        default=False,
+        verbose_name="MAC unknwon",
+        help_text="Use this to create a BMC before the mac address of the machine is known"
+    )
+
     active = models.BooleanField(
         default=True
     )
@@ -566,7 +572,10 @@ class Machine(models.Model):
         if not self.mac_address:
             raise ValidationError("'{}' has no MAC address!".format(self))
 
-        validate_mac_address(self.mac_address)
+        if self.unknown_mac and not self.bmc_allowed():
+            raise ValidationError("You may only skip the MAC for systems with BMC")
+        if self.mac_address:
+            validate_mac_address(self.mac_address)
 
         if not self.system.virtual and self.hypervisor:
             raise ValidationError("Only virtuals machines may have hypervisors")
@@ -692,7 +701,12 @@ class Machine(models.Model):
         return self.installations.get(active=True)
 
     def get_primary_networkinterface(self):
-        return self.networkinterfaces.get(primary=True)
+        try:
+            interface = self.networkinterfaces.get(primary=True)
+        except NetworkInterface.DoesNotExist:
+            logger.debug("In 'get_primary_networkinterface': Machine %s has no networkinterfce", self.fqdn )
+            return None
+        return interface
 
     def get_virtual_machines(self):
         if not self.is_virtual_machine():
